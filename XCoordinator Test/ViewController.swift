963198a86b671f1b3fb18a7064c08c20c0f0d730
Alan <alanpridestar@gmail.com>
2019-08-29 11:06:48 +0300

    Initial Commit

---
+++ XCoordinator Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCoordinator Test
+//
+//  Created by macmini7 on 8/29/19.
+//  Copyright © 2019 macmini7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
